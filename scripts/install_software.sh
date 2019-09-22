29dcdaa3e96caf5029e838a321a675687a95fe3f
avijit dhar <avijit.dhar@edifixio.com>
2019-09-22 09:49:02 +0530

    fix

--- scripts/install_software.sh
+++ scripts/install_software.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install apache2 
-sudo touch /var/www/html/index.html
-sudo echo "Hello World by packer" > /var/www/html/index.html
-sudo systemctl restart apache2 
+#sudo apt-get install apache2 
+#sudo touch /var/www/html/index.html
+#sudo echo "Hello World by packer" > /var/www/html/index.html
+#sudo systemctl restart apache2 
